68c293a79af7c98c45fabf689ac72fd341d11dcb
Hoai-Thu Vuong <thuvh87@gmail.com>
2022-12-13 17:45:24 +0700

    add gpu supporter

--- docker.sh
+++ docker.sh
@@ -25,3 +25,9 @@ if [ -d "/home/thamnth33" ]; then
     sudo usermod -aG docker thamnth33
 fi
 
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
+         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
